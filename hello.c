186b73008cc3eaa92d99340cd0d0ed7e6f8fbd80
Melissa <melissa.futtig@protonmail.com>
2025-03-10 15:21:56 +0100

    added nohting important

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 #include <stdio.h>                      // std = standard i = input o = output
 #include <string.h>                     // for useage of strings
 
-int main(void) {
+int main(void) {                        // 'void' could be left out, bc it has no output to provide
 
     int zahl = 42;                      // first declaration of the int
     zahl = 24;                          // second declaration of the int, but without the the int
@@ -11,14 +11,34 @@ int main(void) {
     long lang = 4;                      // 8 byte, the doubled amount of useage possible
     long grossezahl = 23894213848;
     char zeichen = 'j';                 // shows symbol/ character
-
     int groesse_im_speicher = sizeof(grossezahl);
+    double gehalt = 2343.08972;
+    int a = 23;
+    int b = 3;
+    int ergebnis = a%b;                 // modulo = rest of the full dividation
+    int eingabe = 10;
 
     printf("Hello World \n");
     printf("Zahl: %i", zahl);           // important to include '%i' for integter digit
     printf("\nGroesse im Speicher: %i", groesse_im_speicher);
     printf("\nZeige mir das Symbol: %c", zeichen);  // %i = for integer, %c = character, etc.
+    printf("\nGehalt: %3.2f", gehalt);  // shows number of digits bevor and after the comma
+    printf("\nErgebnis: %i", ergebnis);
     
+    printf("\nBitte gib eine Zahl ein: ");
+
+    // do {
+    //     scanf("%d", &eingabe);              // '&'-operator needs to be set before the scan-input
+    // if (eingabe > 0 && eingabe < 111) { 
+    //     printf("\nFolgende Eingabe: %i", eingabe);
+    // } else { 
+    //     printf("Falsche Eingabe");
+    // } 
+    // } while(eingabe < 0 || eingabe > 111);
     
+    while (1==1) {
+        printf("\nServus!");
+    }
+
     return 0;
 }
